e7f32cade49ff770aada119a1c49b3d37b17edd5
Diana Paievska <71642076+paievska@users.noreply.github.com>
2023-12-05 01:00:32 +0200

    add SharedPreferences

--- app/src/main/java/com/example/cocktails/presentation/fragments/SecondFragment.kt
+++ app/src/main/java/com/example/cocktails/presentation/fragments/SecondFragment.kt
@@ -1,5 +1,6 @@
 package com.example.cocktails.presentation.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.cocktails.R
 import com.example.cocktails.databinding.FragmentSecondBinding
+import android.widget.Toast
 
 class SecondFragment : Fragment() {
     private lateinit var binding: FragmentSecondBinding
@@ -21,7 +23,19 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val sharedPreferences = requireContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
         binding.info.setText(R.string.info)
+        binding.radioButton1.isChecked = sharedPreferences.getBoolean("option1", false)
+        binding.radioButton2.isChecked = sharedPreferences.getBoolean("option2", false)
+        binding.radioButton3.isChecked = sharedPreferences.getBoolean("option3", false)
+        binding.voteBtn.setOnClickListener {
+            with(sharedPreferences.edit()){
+                putBoolean("option1", binding.radioButton1.isChecked)
+                putBoolean("option2", binding.radioButton2.isChecked)
+                putBoolean("option3", binding.radioButton3.isChecked)
+                apply()
+                Toast.makeText(requireContext(), "Thank you for your feedback! And vote save to preference.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
-
 }
